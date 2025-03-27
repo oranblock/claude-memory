e86128531149d5c96152e7a67d75f520efe6a4fe
oranblock <102727189+oranblock@users.noreply.github.com>
2025-03-27 06:09:06 +0300

    Save code artifact: user_modelrb.rb

---
+++ artifacts/user_modelrb.rb
@@ -0,0 +1,16 @@
+# user_model.rb
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_one :profile
+  
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  
+  def self.active_users
+    where(active: true)
+  end
+end
